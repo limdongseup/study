b58f267a4a906b2068e9fe253dd7bdc6443e38af
dongseup <dongseup83@gmail.com>
2016-01-10 15:55:13 +0900

    자바스크립트 성능최적화

---
+++ 자바스크립트_성능최적화/CHAPTER01_로딩과실행/script동적로딩.js
@@ -0,0 +1,25 @@
+function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+
+    if(script.readyState){
+        script.onreadystatechange = function() {
+            if(script.readyState =="loaded" || script.readyState == "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        }
+    } else {
+        script.onload = function() {
+            callback();
+        }
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+// 실행방법
+loadScript("file1.js", function(){
+    alert("file is loaded!");
+});
